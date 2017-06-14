fbf31808ec5301f1518ce6bcd903f96d565d65a4
Sanel <sanel.selmanovic@gmail.com>
2017-06-14 13:50:27 -0400

    Add people list

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -4,16 +4,49 @@
  * @flow
  */
 
- import React, { Component } from 'react';
- import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+import PersonItem from './PersonItem';
 
-export default class PeopleList extends Component {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: 353,
+    flexWrap: 'wrap',
+    paddingTop: 20,
+    paddingLeft: 20,
+  },
+});
+
+class PeopleList extends Component {
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2,
+    });
+    this.dataSource = ds.coloneWithRows(this.props.people);
+  }
 
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Text>I see people!</Text>
+        <ListView
+          enableEmptySections={true}
+          dataSource={this.dataSource}
+          renderRow={(rowData) =>
+            <PersonItem people={rowData} />
+          }
+        />
       </View>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    people: state.people,
+  };
+};
+
+export default connect(mapStateToProps)(PeopleList);
